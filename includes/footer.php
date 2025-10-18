07db17c6a1d3373b4cdec4a80219223675cf48f8
Deepgodhani101 <deepngodhani101@gmail.com>
2025-10-18 18:14:22 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city link">
+                <a href="property_list.php?city=Delhi">PG in Delhi</a>
+            </div>
+            <div class="footer-city link">
+                <a href="property_list.php?city=Mumbai">PG in Mumbai</a>
+            </div>
+            <div class="footer-city link">
+                <a href="property_list.php?city=Gurugram">PG in Gurugram</a>
+            </div>
+            <div class="footer-city link">
+                <a href="property_list.php?city=Vasai">PG in Vasai</a>
+            </div>
+        </div>
+        <div class="VCET">&#169; VCET STUDENTS 2024 2</div>
+    </div>
+</div>
+
+<script type="text/javascript" src="/js/jquery.js"></script>
+<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/js/common.js"></script>
